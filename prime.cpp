4226d54ec2b38926b6bcf7fe01cdb0d0fdbe344a
annov723 <124592563+annov723@users.noreply.github.com>
2023-11-26 19:38:35 +0100

    ready ready ^^

--- prime.cpp
+++ prime.cpp
@@ -3,7 +3,6 @@
 bool is_prime( int x ){
     double check1 = ( double )( x + 1 ) / 6.;
     double check2 = ( double )( x - 1 ) / 6.;
-    cout << "Double1 " << check1 << " i double2 " << check2 << endl;
     if(  x == 2 || x == 3 || ( check1 != 0 && check1 - ( int )check1 == 0 ) || ( check2 != 0 && check2 - ( int )check2 == 0 ) ) return true;
     return false;
 }
@@ -30,9 +29,7 @@ int *find_c( int arr_a[], int arr_b[] ){
     for( int i = 0; arr_a[i] != NULL; i++ ){
         int count = 0;
         for( int j = 0; arr_b[j] != NULL; j++ ) if( arr_b[j] == arr_a[i] ) count++;
-        cout << "Liczby " << arr_a[i] << " mamy " << count << endl;
         if( !is_prime( count ) ){
-            cout << "nie jest pierwsza: " << count << endl;
             arr_c[counter] = arr_a[i];
             counter++;
         }
